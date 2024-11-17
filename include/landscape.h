fdf8c79703cef0f41a63a294b2e2b802594029e6
klukashka <klukin0202@yandex.ru>
2024-11-17 21:30:15 +0300

    some consts for the main file

---
+++ include/landscape.h
@@ -0,0 +1,13 @@
+#define LINE_LENGTH 74
+#define LINES 36
+
+#define PIC_BEGIN 0
+#define PIC_END 29
+
+#define SMOKE_BEGIN 8
+#define SMOKE_END 11
+#define SMOKE_BEGIN1 32
+#define SMOKE_END1 35
+
+#define RIVER_BEGIN 25
+#define RIVER_END 28
